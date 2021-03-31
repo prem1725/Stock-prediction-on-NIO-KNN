b5426edabfbd618c2b8c895fcbf4a7480b00a2eb
prem1725 <73301795+prem1725@users.noreply.github.com>
2021-03-31 13:00:13 -0400

    Add files via upload

---
+++ stockpredictionknn.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 16:25:04 2021
+
+@author: Prem Kumar reddy
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+from matplotlib.pyplot import rcParams
+rcParams['figure.figsize'] = 20,10
+from sklearn import neighbors
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0, 1))
+
+df = pd.read_csv(r'C:\Users\Prem Kumar reddy\Downloads\NIO.csv')
+
+df.head()
+
+df['Date'] = pd.to_datetime(df.Date,format='%Y-%m-%d')
+df.index = df['Date']
+
+plt.figure(figsize=(16,8))
+plt.plot(df['Close'], label='Close Price history')
+
+data = df.sort_index(ascending=True, axis=0)
+new_data = pd.DataFrame(index=range(0,len(df)),columns=['Date', 'Close'])
+
+for i in range(0,len(data)):
+    new_data['Date'][i] = data['Date'][i]
+    new_data['Close'][i] = data['Close'][i]
+
+train = new_data[:210]
+valid = new_data[210:]
+
+print('\n shape of training set: ')
+print(train.shape)
+
+print('\n shape of validation set: ')
+print(valid.shape)
+
+preds = []
+for i in range(0,valid.shape[0]):
+    a = train['Close'][len(train)-43+i:].sum() + sum(preds)
+    b = a/43
+    preds.append(b)
+
+# checking the results (RMSE value)
+rms=np.sqrt(np.mean(np.power((np.array(valid['Close'])-preds),2)))
+print('\n RMSE value on validation set:')
+print(rms)
+
+valid['Predictions'] = 0
+valid['Predictions'] = preds
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+
+#from fastai.structured import add_datepart
+import re
+def add_datepart(df, fldname, drop=True):
+    fld = df[fldname]
+    if not np.issubdtype(fld.dtype, np.datetime64):
+        df[fldname] = fld = pd.to_datetime(fld, infer_datetime_format=True)
+    targ_pre = re.sub('[Dd]ate$', '', fldname)
+    for n in ('Year', 'Month', 'Week', 'Day', 'Dayofweek', 'Dayofyear',
+            'Is_month_end', 'Is_month_start', 'Is_quarter_end', 'Is_quarter_start', 'Is_year_end', 'Is_year_start'):
+        df[targ_pre+n] = getattr(fld.dt,n.lower())
+    df[targ_pre+'Elapsed'] = fld.astype(np.int64) // 10**9
+    if drop: df.drop(fldname, axis=1, inplace=True)
+
+add_datepart(new_data, 'Date')
+new_data.drop('Elapsed', axis=1, inplace=True)
+
+new_data['mon_fri'] = 0
+for i in range(0,len(new_data)):
+    if (new_data['Dayofweek'][i] == 0 or new_data['Dayofweek'][i] == 4):
+        new_data['mon_fri'][i] = 1
+    else:
+        new_data['mon_fri'][i] = 0
+
+train = new_data[:210]
+valid = new_data[210:]
+
+x_train = train.drop('Close', axis=1)
+y_train = train['Close']
+x_valid = valid.drop('Close', axis=1)
+y_valid = valid['Close']
+
+x_train_scaled = scaler.fit_transform(x_train)
+x_train = pd.DataFrame(x_train_scaled)
+x_valid_scaled = scaler.fit_transform(x_valid)
+x_valid = pd.DataFrame(x_valid_scaled)
+
+#using gridsearch to find the best parameter
+params = {'n_neighbors':[2,3,4,5,6,7,8,9]}
+knn = neighbors.KNeighborsRegressor()
+model = GridSearchCV(knn, params, cv=5)
+
+#fit the model and make predictions
+model.fit(x_train,y_train)
+preds = model.predict(x_valid)
+
+rms=np.sqrt(np.mean(np.power((np.array(y_valid)-np.array(preds)),2)))
+rms
+
+valid['Predictions'] = 0
+valid['Predictions'] = preds
+plt.plot(valid[['Close', 'Predictions']])
+plt.plot(train['Close']) 
